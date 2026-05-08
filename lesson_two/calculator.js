0565aeb717e2159f75f9fbf99096b1c893a208a9
dawny <106943353+dawny00@users.noreply.github.com>
2026-05-08 03:05:57 +0000

    Internationalizing the calculator with multi-language support in English, Spanish, and French

--- lesson_two/calculator.js
+++ lesson_two/calculator.js
@@ -1,5 +1,8 @@
 const MESSAGES = require('./calculator_messages.json');
 const readline = require('readline-sync');
+const SUPPORTED_LANGUAGES = ['english', 'spanish', 'french'];
+let language;
+let additionalCalculation;
 
 /*
 
@@ -31,31 +34,50 @@ function calculate(num1, num2, operation) {
   }
 }
 
-prompt(MESSAGES.welcome);
-let additionalCalculation;
+prompt(MESSAGES['english']['welcome']);
+
+// Decide language
+prompt(MESSAGES['english']['language']);
+
+while (true) {
+  language = readline.question().trim().toLowerCase();
+
+  if (!language) {
+    language = 'english';
+    break;
+  }
+
+  if (SUPPORTED_LANGUAGES.includes(language)) {
+    break;
+  }
+
+  if (language && !SUPPORTED_LANGUAGES.includes(language)) {
+    prompt('That is not a supported language yet. Please choose from English, Spanish, or French.');
+  }
+}
 
 do {
   // Gather input
-  prompt(MESSAGES.number1);
+  prompt(MESSAGES[language]['number1']);
   const num1 = Number(readline.question());
 
-  prompt(MESSAGES.number2);
+  prompt(MESSAGES[language]['number2']);
   const num2 = Number(readline.question());
 
-  prompt(MESSAGES.operation);
+  prompt(MESSAGES[language]['operation']);
   const operation = readline.question();
 
   // Perform the operation
   const result = calculate(num1, num2, operation);
 
   if (result === null) {
-    prompt(MESSAGES.invalid);
+    prompt(MESSAGES[language]['invalid']);
   } else {
-    prompt(`${MESSAGES.results} ${result}`);
+    prompt(`${MESSAGES[language]['results']} ${result}`);
   }
 
   // Optionally perform another operation
-  prompt(MESSAGES.anotherOperation);
+  prompt(MESSAGES[language]['anotherOperation']);
   additionalCalculation = readline.question().trim().toUpperCase();
 
-} while (additionalCalculation[0] === "Y");
+} while (additionalCalculation[0] === 'Y');
