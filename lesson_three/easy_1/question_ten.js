2bee013e652f4aa4a58350f773640c94b60e517f
dawny <106943353+dawny00@users.noreply.github.com>
2026-06-18 01:32:30 +0000

    Question ten

---
+++ lesson_three/easy_1/question_ten.js
@@ -0,0 +1,5 @@
+let advice = "Few things in life are as important as house training your pet dinosaur.";
+
+// => My answer:
+console.log(advice.slice(0, advice.indexOf("house")));
+// => 'Few things in life are as important as '
