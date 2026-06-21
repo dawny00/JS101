928e74c3ceef6cc8db59c71bbf5db416b14473cd
dawny <106943353+dawny00@users.noreply.github.com>
2026-06-21 02:56:09 +0000

    Question four

---
+++ lesson_three/easy_2/question_four.js
@@ -0,0 +1,8 @@
+let famousWords = "seven years ago...";
+let prefix = "Four score and ";
+
+// => My answer:
+console.log(prefix + famousWords);
+
+// => My answer without variable:
+console.log(`Four score and ${famousWords}`);
