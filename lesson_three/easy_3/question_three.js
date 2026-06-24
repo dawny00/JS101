10513d80790a8b459dc57d728bb89cd89aa9371d
dawny <106943353+dawny00@users.noreply.github.com>
2026-06-24 04:01:55 +0000

    Question three

---
+++ lesson_three/easy_3/question_three.js
@@ -0,0 +1,12 @@
+let str1 = "hello there";
+let str2 = str1;
+str2 = "goodbye!";
+console.log(str1);
+
+// => What does this code output?
+
+// => My answer:
+// => "hello there"
+
+// => Actual:
+// => "hello there"
