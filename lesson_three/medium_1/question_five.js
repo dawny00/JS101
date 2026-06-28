18fc1e891ca37969c2632d4861645f7db1c9d003
dawny <106943353+dawny00@users.noreply.github.com>
2026-06-28 22:41:37 +0000

    Question five

---
+++ lesson_three/medium_1/question_five.js
@@ -0,0 +1,12 @@
+console.log(0.3 + 0.6);
+console.log(0.3 + 0.6 === 0.9);
+
+// => My answer:
+// 0.9
+// true
+
+// Floting point numbers got me here
+
+// => Actual answer:
+// 0.8999999999999999
+// false
